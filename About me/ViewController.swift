b481a55b79dffc19d43602bbe01dd25ac563d80e
Borys Krisko <bvkr@me.com>
2019-08-02 06:19:13 +0300

    Initial Commit

---
+++ About me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Борис Крисько on 8/2/19.
+//  Copyright © 2019 Borys Krisko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
