6fcd800f2c0b86a9173bddf992dd3e7f8de2f28e
Borys Krisko <bvkr@me.com>
2019-08-02 15:43:15 +0300

    working with interface builder

---
+++ About me/HobbyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HobbyViewController.swift
+//  About me
+//
+//  Created by Борис Крисько on 8/2/19.
+//  Copyright © 2019 Borys Krisko. All rights reserved.
+//
+
+import UIKit
+
+class HobbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
